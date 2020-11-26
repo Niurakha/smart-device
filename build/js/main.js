c0f0feb3bfd07a84c3061ba84a296fe2d010db9b
Niurakha <proshinaann89@gmail.com>
2020-11-26 23:31:45 +0300

    Добавялет "аккордеон"

--- build/js/main.js
+++ build/js/main.js
@@ -1,21 +1,38 @@
 'use strict';
 
-// скролл по якроной ссылке
+(function () {
 
-var anchors = document.querySelectorAll('a[href*="#"]');
+  // аккордеон
+  var accordionItems = document.querySelectorAll('.accordion');
+  var accordionBlocks = document.querySelectorAll('.accordion__block');
 
-anchors.forEach(function (anchor) {
-  anchor.addEventListener('click', function (evt) {
-    evt.preventDefault();
+  var hideBlock = function (button, block) {
+    button.classList.add('accordion__toggle--inactive');
+    block.classList.add('accordion__block--hidden');
+  };
 
-    var blockID = anchor.getAttribute('href').substring(1);
+  var showBlock = function (button, block) {
+    button.classList.remove('accordion__toggle--inactive');
+    block.classList.remove('accordion__block--hidden');
+  };
 
-    document.getElementById(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
+  var toggleAccordion = function (evt) {
+    Array.prototype.forEach.call(accordionBlocks, function (accordionBlock) {
+      var button = accordionBlock.closest('.accordion').querySelector('.accordion__toggle');
+      if (button === evt.target && !button.classList.contains('accordion__toggle--inactive') || button !== evt.target) {
+        hideBlock(button, accordionBlock);
+      } else if (button === evt.target) {
+        showBlock(button, accordionBlock);
+      }
     });
+  };
+
+  Array.prototype.forEach.call(accordionItems, function (accordion) {
+    var accordionToggleButton = accordion.querySelector('.accordion__toggle');
+    var accordionBlock = accordion.querySelector('.accordion__block');
+    hideBlock(accordionToggleButton, accordionBlock);
+    accordionToggleButton.addEventListener('click', toggleAccordion);
   });
-});
 
 // маска для номера телефона
 
