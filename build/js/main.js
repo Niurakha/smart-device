381720aa1c6288f6432a6c4db7bc7e73bb057b16
Niurakha <proshinaann89@gmail.com>
2020-11-26 23:34:06 +0300

    Добавялет "маску" и скролл

--- build/js/main.js
+++ build/js/main.js
@@ -34,9 +34,25 @@
     accordionToggleButton.addEventListener('click', toggleAccordion);
   });
 
-// маска для номера телефона
+  // скролл по якорной ссылке
 
-window.addEventListener('DOMContentLoaded', function () {
+  var anchors = document.querySelectorAll('a[href*="#"]');
+
+  anchors.forEach(function (anchor) {
+    anchor.addEventListener('click', function (evt) {
+      evt.preventDefault();
+
+      var blockID = anchor.getAttribute('href').substring(1);
+
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    });
+  });
+
+  // маска для номера телефона
+
   function setCursorPosition(pos, elem) {
     elem.focus();
     if (elem.setSelectionRange) {
@@ -50,38 +66,43 @@ window.addEventListener('DOMContentLoaded', function () {
     }
   }
 
-  var input = document.querySelector('#tel');
-  input.addEventListener('input', mask, false);
-  input.addEventListener('focus', mask, false);
-  input.addEventListener('blur', mask, false);
+  var maskedInputs = document.querySelectorAll('input[name="phone"]');
 
-  function mask(event) {
-    var matrix = '+7 (___) ___-__-__';
-    var i = 0;
-    var def = matrix.replace(/\D/g, '');
-    var val = input.value.replace(/\D/g, '');
+  Array.prototype.forEach.call(maskedInputs, function (input) {
+    input.addEventListener('input', mask, false);
+    input.addEventListener('focus', mask, false);
+    input.addEventListener('blur', mask, false);
 
-    if (def.length >= val.length) {
-      val = def;
-    }
+    function mask(event) {
+      var matrix = '+7 (___) ___-__-__';
+      var i = 0;
+      var def = matrix.replace(/\D/g, '');
+      var val = input.value.replace(/\D/g, '');
 
-    input.value = matrix.replace(/./g, function (a) {
-      if (/[_\d]/.test(a) && i < val.length) {
-        return val.charAt(i++);
-      } else if (i >= val.length) {
-        return '';
+      if (def.length >= val.length) {
+        val = def;
+      }
+
+      input.value = matrix.replace(/./g, function (a) {
+        if (/[_\d]/.test(a) && i < val.length) {
+          return val.charAt(i++);
+        } else if (i >= val.length) {
+          return '';
+        } else {
+          return a;
+        }
+      });
+
+      if (event.type === 'blur') {
+        if (input.value.length === 2) {
+          input.value = '';
+        }
       } else {
-        return a;
+        setCursorPosition(input.value.length, input);
       }
-    });
-
-    if (event.type === 'blur') {
-      if (input.value.length === 2) {
-        input.value = '';
-      }
-    } else {
-      setCursorPosition(input.value.length, input);
     }
+  });
+
   // popup
   var KEYCODE = {
     esc: 27
